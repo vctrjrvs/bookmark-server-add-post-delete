a05d0dcb3ff27a728aa5cc684095a5572a44eb9c
Victor Jarvis <vctrjrvs@gmail.com>
2020-06-05 13:51:41 -0400

    Initial and modules

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+     PORT: process.env.PORT || 8000,
+     NODE_ENV: process.env.NODE_ENV || 'development',
+     API_TOKEN: process.env.API_TOKEN || '16cd321e-ad59-461f-93c1-e9eafd344c9e',
+   }
